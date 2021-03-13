a8ec2de11347d39229892dc5e1b6534e3c6f3206
Filipe Tavares-Cadete <filipe@cadete.eu>
2021-03-13 23:00:36 +0000

    Runs salmon on first documentID

--- workspace.R
+++ workspace.R
@@ -1,11 +1,37 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "994a884b38f62643bc46779ff4006817")
+options("tercen.stepId"     = "87257e37-de66-48e7-8a7d-ece2881d041f")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
+
+
+ctx = tercenCtx()
+
+if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required") 
+
+df <- ctx$cselect()
+
+docId = df$documentId[1]
+doc = ctx$client$fileService$get(docId)
+filename = paste0(tempfile(), '.fastq')
+writeBin(ctx$client$fileService$download(docId), filename)
+on.exit(unlink(filename))
+
+out.filename = tempfile()
+
+cmd = 'salmon'
+args = paste('quant',
+             '-i', 'hsapiens_index', 
+             '-l A',
+             '-r', filename,
+             '-p', parallel::detectCores(),
+             '--validateMappings',
+             '-o', out.filename,
+             sep = ' ')
+system2(cmd, args)
 
 (ctx = tercenCtx())  %>% 
   select(.y, .ci, .ri) %>% 
