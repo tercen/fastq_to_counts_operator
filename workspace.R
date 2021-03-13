b4fef61b8dc230812d2158351a1c84c97ebf0e55
Filipe Tavares-Cadete <filipe@cadete.eu>
2021-03-13 23:24:35 +0000

    Now works for single- and paired-end sequencing.

--- workspace.R
+++ workspace.R
@@ -12,30 +12,61 @@ ctx = tercenCtx()
 
 if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required") 
 
+if (length(df$documentId) > 2) stop("Can only have one documentID if single-ended or two if paired-ended sequencing") 
+
 df <- ctx$cselect()
 
-docId = df$documentId[1]
-doc = ctx$client$fileService$get(docId)
-filename = paste0(tempfile(), '.fastq')
-writeBin(ctx$client$fileService$download(docId), filename)
-on.exit(unlink(filename))
+if (length(df$documentId) == 2) {
+  
+  docId = df$documentId[1]
+  doc = ctx$client$fileService$get(docId)
+  filename_r1 = paste0(tempfile(), '.fastq')
+  writeBin(ctx$client$fileService$download(docId), filename_r1)
+  on.exit(unlink(filename_r1))
+  
+  docId = df$documentId[2]
+  doc = ctx$client$fileService$get(docId)
+  filename_r2 = paste0(tempfile(), '.fastq')
+  writeBin(ctx$client$fileService$download(docId), filename_r2)
+  on.exit(unlink(filename_r2))
+  
+  out.filename = tempfile()
+  
+  cmd = 'salmon'
+  args = paste('quant',
+               '-i', 'hsapiens_index', 
+               '-l A',
+               '-1', filename_r1,
+               '-2', filename_r2,
+               '-p', parallel::detectCores(),
+               '--validateMappings',
+               '-o', out.filename,
+               sep = ' ')
+} else if (length(df$documentId) == 1) {
+  
+  docId = df$documentId[1]
+  doc = ctx$client$fileService$get(docId)
+  filename = paste0(tempfile(), '.fastq')
+  writeBin(ctx$client$fileService$download(docId), filename)
+  on.exit(unlink(filename))
+  
+  out.filename = tempfile()
+  
+  cmd = 'salmon'
+  args = paste('quant',
+               '-i', 'hsapiens_index', 
+               '-l A',
+               '-r', filename,
+               '-p', parallel::detectCores(),
+               '--validateMappings',
+               '-o', out.filename,
+               sep = ' ')
+}
 
-out.filename = tempfile()
-
-cmd = 'salmon'
-args = paste('quant',
-             '-i', 'hsapiens_index', 
-             '-l A',
-             '-r', filename,
-             '-p', parallel::detectCores(),
-             '--validateMappings',
-             '-o', out.filename,
-             sep = ' ')
 system2(cmd, args)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+salmon_quant <- read.delim(paste(out.filename, 'quant.sf', sep = '/'))
+
+(ctx = salmon_quant %>%
   ctx$addNamespace() %>%
-  ctx$save()
+  ctx$save())
